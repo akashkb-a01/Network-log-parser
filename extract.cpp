cdc6060918f6b93ffa1cd314754790ffd1e6ad41
akashkb-a01 <75172396+akashkb-a01@users.noreply.github.com>
2022-12-19 00:05:14 +0530

    Added Source and Destination

--- extract.cpp
+++ extract.cpp
@@ -10,12 +10,11 @@ int main(int argc, char const *argv[])
     ifstream inFile(inpfile);
     ofstream outFile(outpfile);
     string line;
-    outFile << "PktNumber,Appearance,Direction,Type,Component,Edge,Filter,OriginalSize,LoggedSize" << '\n';
-
-    string m[] = {"PktNumber","Appearance","Direction","Type","Component","Edge","Filter","OriginalSize","LoggedSize"};
+    outFile << "PktNumber,Appearance,Direction,Type,Component,Edge,Filter,OriginalSize,LoggedSize,Source,Destination" << '\n';
+    string m[] = {"PktNumber","Appearance","Direction","Type","Component","Edge","Filter","OriginalSize","LoggedSize","Source","Destination"};
     while (inFile)
     {
-       string token;
+       string type;
        string t = "";
        int pos = 0;
        while(getline(inFile, line)){
@@ -26,13 +25,52 @@ int main(int argc, char const *argv[])
             if(fou != string::npos){
                 pos = fou;
                 pos = line.find(", ", pos);
+                if(i == 3)
+                    type = line.substr(fou + m[i].length() + 1, pos - (fou + m[i].length() + 2 ));
                 t += line.substr(fou + m[i].length() + 1, pos - (fou + m[i].length() + 1));
             }
-            if(i!=8)
-                t += ",";
+            t += ",";
         }
-        if(t != ",,, 0,,,,," && t != ",,,,,,,,")
+        if(t != ",,, 0,,,,,," && t != ",,,,,,,,,"){
+            if(type == "IP"){
+                int pos, fou;
+                getline(inFile, line);
+                fou = line.find("ip: ", 0);
+                pos = fou + 3;
+                fou = line.find(">", pos);
+                t += (line.substr(pos + 1, fou - pos - 2) + ",");
+                pos = fou + 1;
+                fou = line.find(": ", pos);
+                t += line.substr(pos + 1, fou - pos - 1);
+            }
+            else if(type == "Ethernet"){
+                int pos, fou;
+                getline(inFile, line);
+                fou = line.find(": ", 0);
+                pos = fou + 1;
+                fou = line.find(">", pos);
+                t += (line.substr(pos + 1, fou - pos - 2) + ",");
+                pos = fou + 1;
+                fou = line.find(": ", pos);
+                t += line.substr(pos + 1, fou - pos - 1);
+            }
+
+            else if(type == "WiFi"){
+                int pos, fou;
+                getline(inFile, line);
+                fou = line.find("SA:", 0);
+                pos = fou + 2;
+                fou = line.find(" ", pos);
+                t += (line.substr(pos + 1, fou - pos - 1) + ",");
+                fou = line.find("DA:", 0);
+                pos = fou + 2;
+                fou = line.find(" ", pos);
+                t += (line.substr(pos + 1, fou - pos - 1) + ",");
+            }
+            else 
+            t += ",";
             outFile << t << endl;
+        }
        }
     }
 
